4f85da642e79bb8b9e08e27af0ec5f5381067b17
rileyeh <rileyehatch@gmail.com>
2019-06-18 22:57:53 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <nav className="navbar">
+          <div className="nav-container">
+            <div className='menu-closed'>
+              <h4>Start Bootstrap</h4>
+              <label for="toggle" class="label">MENU &#9776;</label>
+            </div>
+
+            <div className="menu-open">
+              <input type="checkbox" id="toggle"/>
+              <div className="nav-links">
+                <a href="#">SERVICES</a>
+                <a href="#">PORTFOLIO</a>
+                <a href="#">ABOUT</a>
+                <a href="#">TEAM</a>
+                <a href="#">CONTACT</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div class="welcome-box">
+          <div className="welcome-text">
+            <h3>Welcome To Our Studio!</h3>
+          </div>
+
+          <div className="big-text">
+            <h1>IT'S NICE TO MEET YOU</h1>
+          </div>
+
+          <button>
+            TELL ME MORE
+          </button>
+            
+        </div>
+    </header>
     </div>
   );
 }
